0eeaeb385018d30c921ae9858ac35fef5cc7e872
Cinquin Andy <andy.cinquin@infotel.com>
2023-04-23 13:26:20 +0200

    set main page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html lang="en">
-			<Head />
+		<Html lang="fr">
+			<Head title={'ForHives'}>
+				<meta
+					name="description"
+					content="Rejoignez ForHives, La communauté où chaque ruche est un projet, chaque idée est une abeille,
+					et la passion est la reine qui les gouverne. Travaillons ensemble pour donner vie à vos projets et changer le monde !"
+				/>
+				{/*	ico */}
+				<link rel="icon" href="/ForHives.webp" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
