d2bc55383764ce6c9bce520d4ff58b4636ec6a7c
CinquinAndy <cinquin.andy@gmail.com>
2024-03-07 09:02:06 +0100

    ✨ Refactor image rendering in Home component
    Simplified the return statement by removing unnecessary JSX fragments and improving code readability.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import Image from 'next/image'
-import { Head } from 'next/document'
 
 export default function Home() {
 	return (
-		<>
-			<main className={'relative h-screen w-screen'}>
-				<Image
-					src={'/ForHives.svg'}
-					alt={'ForHives'}
-					width={'250'}
-					height={'250'}
-					className={
-						'fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform'
-					}
-				/>
-			</main>
-		</>
+		<main className={'relative h-screen w-screen'}>
+			<Image
+				src={'/ForHives.svg'}
+				alt={'ForHives'}
+				width={'250'}
+				height={'250'}
+				className={
+					'fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform'
+				}
+			/>
+		</main>
 	)
 }
