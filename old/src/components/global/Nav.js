7310ced8d267a95ae3b74c6cb26f82da37ef3c73
Cinquin Andy <andy.cinquin@infotel.com>
2024-06-21 22:28:39 +0200

    ✨ Update file paths and dependencies
    - Renamed and updated file paths for build workflows, Dockerfile, and copied files.
    - Removed outdated ESLint configuration, Gitignore rules, Husky pre-commit script, JSConfig, and Next.js config.
    - Deleted old package-lock.json with dependencies.

--- old/src/components/global/Nav.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Nav(props) {
-	return <div>nav</div>
-}
-
-export default Nav
--- old/src/pages/_app.js
+++
@@ -1,5 +0,0 @@
-import 'old/src/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
-}
