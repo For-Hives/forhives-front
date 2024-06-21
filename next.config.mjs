a5548428295c648c75e8f9c1358d99c6e1123df5
Cinquin Andy <andy.cinquin@infotel.com>
2024-06-21 22:20:09 +0200

    ✨ Update ESLint configuration and file paths
    - Updated ESLint configuration to include additional plugins and rules.
    - Refactored file paths for MDX related imports.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 import nextMDX from '@next/mdx'
 
-import { recmaPlugins } from './mdx/recma.mjs'
-import { rehypePlugins } from './mdx/rehype.mjs'
-import { remarkPlugins } from './mdx/remark.mjs'
+import { recmaPlugins } from '@/mdx/recma.mjs'
+import { rehypePlugins } from '@/mdx/rehype.mjs'
+import { remarkPlugins } from '@/mdx/remark.mjs'
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
